6eee18bd10475c313c11e742f094969249283b9d
jessey <alexei.iljin@gmail.com>
2012-11-06 00:11:43 +0400

    sitetree and left_menu added

--- trailers_shop/settings.py
+++ trailers_shop/settings.py
@@ -92,6 +92,18 @@ class CommonSettings(DjangoDefaults):
         'django.contrib.admindocs',
         'django.contrib.flatpages',
         'django.contrib.staticfiles',
+        'sitetree',
+    ]
+
+    TEMPLATE_CONTEXT_PROCESSORS = [
+        "django.contrib.auth.context_processors.auth",
+        "django.core.context_processors.request",
+        "django.core.context_processors.debug",
+        "django.core.context_processors.i18n",
+        "django.core.context_processors.media",
+        "django.core.context_processors.static",
+        "django.core.context_processors.tz",
+        "django.contrib.messages.context_processors.messages",
     ]
 
     LOGGING = {
