300d8b0fb3336a74e7fd749a97611ae5ad720a40
clincher <va.bolshakov@gmail.com>
2013-03-28 08:12:24 +0700

    fix settings

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
+import os
 import sys
-import cbsettings
 
 
 if __name__ == "__main__":
-    cbsettings.configure("atrailer.settings.Settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "atrailer.settings")
 
     from django.core.management import execute_from_command_line
 
